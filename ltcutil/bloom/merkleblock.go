3c78cbb56864359f9412cee0d7db2085fe00dc62
Hector Chu <hectorchu@gmail.com>
2024-01-10 18:03:38 +0000

    Add function to verify merkle blocks

--- ltcutil/bloom/merkleblock.go
+++ ltcutil/bloom/merkleblock.go
@@ -21,6 +21,15 @@ type merkleBlock struct {
 	bits        []byte
 }
 
+type merkleExtractResult struct {
+	root     *chainhash.Hash
+	match    []*chainhash.Hash
+	index    []uint32
+	bitsUsed int
+	hashUsed int
+	bad      bool
+}
+
 // calcTreeWidth calculates and returns the number of nodes (width) or a
 // merkle tree at the given depth-first height.
 func (m *merkleBlock) calcTreeWidth(height uint32) uint32 {
@@ -78,6 +87,111 @@ func (m *merkleBlock) traverseAndBuild(height, pos uint32) {
 	}
 }
 
+func (m *merkleBlock) traverseAndExtract(height, pos uint32,
+	res *merkleExtractResult) *chainhash.Hash {
+
+	if res.bitsUsed >= len(m.bits) {
+		// Overflowed the bits array - failure
+		res.bad = true
+		return &chainhash.Hash{}
+	}
+	parentOfMatch := m.bits[res.bitsUsed] > 0
+	res.bitsUsed++
+
+	if height == 0 || !parentOfMatch {
+		// If at height 0, or nothing interesting below,
+		// use stored hash and do not descend
+
+		if res.hashUsed >= len(m.finalHashes) {
+			// Overflowed the hash array - failure
+			res.bad = true
+			return &chainhash.Hash{}
+		}
+		hash := m.finalHashes[res.hashUsed]
+		res.hashUsed++
+
+		if height == 0 && parentOfMatch {
+			// In case of height 0, we have a matched txid
+			res.match = append(res.match, hash)
+			res.index = append(res.index, pos)
+		}
+		return hash
+
+	} else {
+		// Otherwise, descend into the subtrees to extract
+		// matched txids and hashes
+		left := m.traverseAndExtract(height-1, pos*2, res)
+		right := left
+
+		if pos*2+1 < m.calcTreeWidth(height-1) {
+			right = m.traverseAndExtract(height-1, pos*2+1, res)
+
+			if left.IsEqual(right) {
+				// The left and right branches should never be
+				// identical, as the transaction hashes covered
+				// by them must each be unique.
+				res.bad = true
+			}
+		}
+
+		// Combine them before returning
+		return blockchain.HashMerkleBranches(left, right)
+	}
+}
+
+func (m *merkleBlock) extractMatches() (res *merkleExtractResult) {
+	res = &merkleExtractResult{}
+
+	// An empty set will not work
+	if m.numTx == 0 {
+		return
+	}
+
+	// Check for excessively high numbers of transactions
+	if m.numTx > blockchain.MaxOutputsPerBlock {
+		return
+	}
+
+	// There can never be more hashes provided than one for every txid
+	if uint32(len(m.finalHashes)) > m.numTx {
+		return
+	}
+
+	// There must be at least one bit per node in the partial tree,
+	// and at least one node per hash
+	if len(m.bits) < len(m.finalHashes) {
+		return
+	}
+
+	// Calculate height of tree
+	var height uint32
+	for m.calcTreeWidth(height) > 1 {
+		height++
+	}
+
+	// Traverse the partial tree
+	hashMerkleRoot := m.traverseAndExtract(height, 0, res)
+
+	// Verify that no problems occurred during the tree traversal
+	if res.bad {
+		return
+	}
+
+	// Verify that all bits were consumed (except for the padding
+	// caused by serializing it as a byte sequence)
+	if (res.bitsUsed+7)/8 != (len(m.bits)+7)/8 {
+		return
+	}
+
+	// Verify that all hashes were consumed
+	if res.hashUsed != len(m.finalHashes) {
+		return
+	}
+
+	res.root = hashMerkleRoot
+	return
+}
+
 // NewMerkleBlock returns a new *wire.MsgMerkleBlock and an array of the matched
 // transaction index numbers based on the passed block and filter.
 func NewMerkleBlock(block *ltcutil.Block, filter *Filter) (*wire.MsgMerkleBlock, []uint32) {
@@ -123,4 +237,17 @@ func NewMerkleBlock(block *ltcutil.Block, filter *Filter) (*wire.MsgMerkleBlock,
 		msgMerkleBlock.Flags[i/8] |= mBlock.bits[i] << (i % 8)
 	}
 	return &msgMerkleBlock, matchedIndices
+}
+
+// VerifyMerkleBlock verifies the integrity of a merkle block.
+func VerifyMerkleBlock(msgMerkleBlock *wire.MsgMerkleBlock) bool {
+	mBlock := &merkleBlock{
+		numTx:       msgMerkleBlock.Transactions,
+		finalHashes: msgMerkleBlock.Hashes,
+		bits:        make([]byte, len(msgMerkleBlock.Flags)*8),
+	}
+	for i := range mBlock.bits {
+		mBlock.bits[i] = msgMerkleBlock.Flags[i/8] & (1 << (i % 8))
+	}
+	return mBlock.extractMatches().root.IsEqual(&msgMerkleBlock.Header.MerkleRoot)
 }
