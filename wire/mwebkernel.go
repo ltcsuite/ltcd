0d412ac7890ae11a0d2ef2607b10d6ca4994814a
Hector Chu <hectorchu@gmail.com>
2024-01-26 13:09:52 +0000

    Add mweb kernel

---
+++ wire/mwebkernel.go
@@ -0,0 +1,160 @@
+package wire
+
+import (
+	"errors"
+	"io"
+
+	"github.com/ltcsuite/ltcd/chaincfg/chainhash"
+	"github.com/ltcsuite/ltcd/ltcutil/mweb/mw"
+	"lukechampine.com/blake3"
+)
+
+const (
+	MwebKernelFeeFeatureBit MwebKernelFeatureBit = 1 << iota
+	MwebKernelPeginFeatureBit
+	MwebKernelPegoutFeatureBit
+	MwebKernelHeightLockFeatureBit
+	MwebKernelStealthExcessFeatureBit
+	MwebKernelExtraDataFeatureBit
+
+	MwebKernelAllFeatureBits = MwebKernelFeeFeatureBit |
+		MwebKernelPeginFeatureBit | MwebKernelPegoutFeatureBit |
+		MwebKernelHeightLockFeatureBit | MwebKernelStealthExcessFeatureBit |
+		MwebKernelExtraDataFeatureBit
+)
+
+type (
+	MwebKernelFeatureBit byte
+
+	MwebKernel struct {
+		Features      MwebKernelFeatureBit
+		Fee           uint64
+		Pegin         uint64
+		Pegouts       []TxOut
+		LockHeight    int32
+		StealthExcess mw.PublicKey
+		ExtraData     []byte
+		Excess        mw.Commitment
+		Signature     mw.Signature
+	}
+)
+
+func (mk *MwebKernel) Hash() *chainhash.Hash {
+	h := blake3.New(32, nil)
+	mk.write(h, 0)
+	return (*chainhash.Hash)(h.Sum(nil))
+}
+
+// Reads a litecoin mweb kernel from r.  See Deserialize for
+// decoding mweb kernels stored to disk, such as in a database,
+// as opposed to decoding from the wire.
+func (mk *MwebKernel) read(r io.Reader, pver uint32) error {
+	err := readElements(r, &mk.Features)
+	if err != nil {
+		return err
+	}
+
+	if mk.Features&MwebKernelFeeFeatureBit > 0 {
+		if mk.Fee, err = readVarInt(r); err != nil {
+			return err
+		}
+	}
+
+	if mk.Features&MwebKernelPeginFeatureBit > 0 {
+		if mk.Pegin, err = readVarInt(r); err != nil {
+			return err
+		}
+	}
+
+	if mk.Features&MwebKernelPegoutFeatureBit > 0 {
+		var count uint64
+		if count, err = ReadVarInt(r, pver); err != nil {
+			return err
+		}
+		if count > maxTxOutPerMessage {
+			return errors.New("count too large")
+		}
+		mk.Pegouts = make([]TxOut, count)
+		for i := range mk.Pegouts {
+			if err = ReadTxOut(r, pver, 0, &mk.Pegouts[i]); err != nil {
+				return err
+			}
+		}
+	}
+
+	if mk.Features&MwebKernelHeightLockFeatureBit > 0 {
+		var lockHeight uint64
+		if lockHeight, err = readVarInt(r); err != nil {
+			return err
+		}
+		mk.LockHeight = int32(lockHeight)
+	}
+
+	if mk.Features&MwebKernelStealthExcessFeatureBit > 0 {
+		if _, err = io.ReadFull(r, mk.StealthExcess[:]); err != nil {
+			return err
+		}
+	}
+
+	if mk.Features&MwebKernelExtraDataFeatureBit > 0 {
+		mk.ExtraData, err = ReadVarBytes(r, pver, MaxMessagePayload, "ExtraData")
+		if err != nil {
+			return err
+		}
+	}
+
+	return readElements(r, mk.Excess[:], mk.Signature[:])
+}
+
+// Writes a litecoin mweb kernel to w.  See Serialize for
+// encoding mweb kernels to be stored to disk, such as in
+// a database, as opposed to encoding for the wire.
+func (mk *MwebKernel) write(w io.Writer, pver uint32) error {
+	err := writeElements(w, mk.Features)
+	if err != nil {
+		return err
+	}
+
+	if mk.Features&MwebKernelFeeFeatureBit > 0 {
+		if err = writeVarInt(w, mk.Fee); err != nil {
+			return err
+		}
+	}
+
+	if mk.Features&MwebKernelPeginFeatureBit > 0 {
+		if err = writeVarInt(w, mk.Pegin); err != nil {
+			return err
+		}
+	}
+
+	if mk.Features&MwebKernelPegoutFeatureBit > 0 {
+		if err = WriteVarInt(w, pver, uint64(len(mk.Pegouts))); err != nil {
+			return err
+		}
+		for i := range mk.Pegouts {
+			if err = WriteTxOut(w, pver, 0, &mk.Pegouts[i]); err != nil {
+				return err
+			}
+		}
+	}
+
+	if mk.Features&MwebKernelHeightLockFeatureBit > 0 {
+		if err = writeVarInt(w, uint64(mk.LockHeight)); err != nil {
+			return err
+		}
+	}
+
+	if mk.Features&MwebKernelStealthExcessFeatureBit > 0 {
+		if _, err = w.Write(mk.StealthExcess[:]); err != nil {
+			return err
+		}
+	}
+
+	if mk.Features&MwebKernelExtraDataFeatureBit > 0 {
+		if err = WriteVarBytes(w, pver, mk.ExtraData); err != nil {
+			return err
+		}
+	}
+
+	return writeElements(w, mk.Excess[:], mk.Signature[:])
+}
