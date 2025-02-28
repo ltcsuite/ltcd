269a224e557bf2acf21b1eb6b41b93276c1e41c1
Hector Chu <hectorchu@gmail.com>
2025-02-28 18:28:35 +0000

    Scrypt ARM64 assembly
    20% faster than C on mobile

--- ltcutil/scrypt/scrypt.go
+++ ltcutil/scrypt/scrypt.go
@@ -1,16 +1,10 @@
+//go:build !arm64
+
 package scrypt
 
-// void scrypt_aux(unsigned char*);
-import "C"
-
-import (
-	"crypto/sha256"
-
-	"golang.org/x/crypto/pbkdf2"
-)
+import scrypt2 "golang.org/x/crypto/scrypt"
 
 func Scrypt(x []byte) []byte {
-	X := pbkdf2.Key(x, x, 1, 128, sha256.New)
-	C.scrypt_aux((*C.uchar)(&X[0]))
-	return pbkdf2.Key(x, X, 1, 32, sha256.New)
+	x, _ = scrypt2.Key(x, x, 1024, 1, 1, 32)
+	return x
 }
